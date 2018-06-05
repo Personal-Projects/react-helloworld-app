7684630c3bde3a11b817c5618e7c7ce84d8f6914
jasmine <jasliu2012@gmail.com>
2018-06-05 16:02:28 -0500

    Simple Hello World app setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// tell JavaScript to import React library from the installed 'react' NPM module.
+import React from 'react';
+// tell JavaScript to import the file App.css from the current folder indicated with '.'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// const is a constant function that cannot be changed.
+const App = () => {
+  // All functional components return the JSX that tells React how to create a component.
+  // JSX is templating language that look like HTML. Wrapped in {}, reserved word className.
+  // React components must only return a SINGLE JSX node at its root! So wrap components that might have multiple children in single div.
+  return <div className="App">Hello World!</div>;
+};
 
+// export the code for other files to use
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+// tell JavaScript to import React library from the installed 'react' NPM module.
 import React from 'react';
+// tell JavaScript that we want the ReactDOM library out of the 'react-dom' NPM module
+// ReactDOM allow use of render(), which tells React precisely HOW to throw a component into the browser.
 import ReactDOM from 'react-dom';
+// tell JavaScript to import the index.css file from current folder, which is our main global CSS file.
 import './index.css';
+// tell JavaScript to import the App component from a local file called "App.js". ES2015 knows any file beginning ./ is local, can leave off the .js
 import App from './App';
+// tell JavaScript to allow us access to implementing service workers for progressive web apps in our create react app.
 import registerServiceWorker from './registerServiceWorker';
 
+// render call: Render is a function that takes two arguments 1) which component to render, and 2) where to render that component.
+// declare component using JSX as <App />
+// where to render: document.getElementById('root'). Tells JavaScript to find an element on page which has an ID of 'root'
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
