529c0eafffaebdea7dcdf919967c81f5588d79db
jasmine <jasliu2012@gmail.com>
2018-06-05 16:11:53 -0500

    Separated HelloWorld into its own component.

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,22 +1,15 @@
 // tell JavaScript to import React library from the installed 'react' NPM module.
 import React from 'react';
 // tell JavaScript to import the file App.css from the current folder indicated with '.'
-import './App.css';
-// tell JavaScript to import the HelloWorld component into App.js
-import HelloWorld from './HelloWorld';
-// const is a constant function that cannot be changed.
-const App = () => {
+import './HelloWorld.css';
+
+// const is a constant function that cannot be changed and doesn't accept arguments.
+const HelloWorld = () => {
   // All functional components return the JSX that tells React how to create a component.
   // JSX is templating language that look like HTML. Wrapped in {}, reserved word className.
   // React components must only return a SINGLE JSX node at its root! So wrap components that might have multiple children in single div.
-  // root div renders a child HelloWorld component
-  return (
-    <div className="App">
-      <HelloWorld />
-      <HelloWorld />
-    </div>
-  );
+  return <div className="HelloWorld">Hello World!</div>;
 };
 
 // export the code for other files to use
-export default App;
+export default HelloWorld;
